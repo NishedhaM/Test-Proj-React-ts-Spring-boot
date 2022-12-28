6d7d6a4e3b65e94bb754781c1f4c1c3ed310453a
NishedhaM <Kadnishedha@gmail.com>
2022-12-28 14:07:15 +0530

    Finishing the registration form

--- frontend/tp-front/src/App.tsx
+++ frontend/tp-front/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
 import LoginMe from './Pages/Login';
+import RegisterMe from './Pages/Register';
 
 function App() {
   return (
     <div className="App">
         {/* <Basic/> */}
-        <LoginMe/>
+        {/* <LoginMe/> */}
+        <RegisterMe/>
+
     </div>
   );
 }
---
+++ frontend/tp-front/src/Pages/Register.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Breadcrumb, Layout, Menu, Space, theme } from 'antd';
+import HeaderMe from '../Components/Header';
+import FooterMe from '../Components/Footer';
+import { Button, Checkbox, Form, Input } from 'antd';
+import { Typography } from 'antd';
+
+const { Text, Link } = Typography;
+
+const { Header, Content, Footer } = Layout;
+
+const RegisterMe: React.FC = () => {
+    const onFinish = (values: any) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
+
+    return (
+        <Layout>
+            <HeaderMe />
+            <h1 style={{ marginTop: 100, display: 'flex', justifyContent: 'center', fontFamily: 'cursive' }}>Registration Form</h1>
+            <Content className="site-layout" style={{ padding: '0 50px', display: 'flex', justifyContent: 'center', marginTop: 40 }}>
+                <Form
+                    name="basic"
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                >
+                    <Form.Item
+                        label="Username"
+                        name="username"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+
+                    <Form.Item
+                        label="ConfirmPassword"
+                        name="ConfirmPassword"
+                        rules={[{ required: true, message: 'Please confirm your password!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                        <Button type="primary" htmlType="submit" style={{backgroundColor:'#090850',display: 'flex', justifyContent: 'center'}}>
+                            Register
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Content>
+            <Space direction='vertical'>
+                        <Text>Already have an account?<Link href="https://ant.design" target="_blank">Login</Link></Text>
+            </Space>
+            
+            <div style={{
+                marginTop:129,
+                
+            }}><FooterMe /></div>
+
+        </Layout>
+    );
+};
+
+export default RegisterMe;
